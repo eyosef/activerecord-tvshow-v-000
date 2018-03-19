8263dfb4a61a8436efcdc5dd86dcd1ebf08fac6e
Eden Mesfun <emesfun@gmail.com>
2018-03-19 13:45:59 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,8 +1,8 @@
 class CreateShoes < ActiveRecord::Migration
   def change
-    create_table :shows do |t| (
+    create_table :shows do |t| 
     string :name
     string :network
     string :day
     integer :rating
-    )
+    
