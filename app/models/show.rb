8ff6b8aa8fba7198de6140e3c64baa40aa749064
Eden Mesfun <emesfun@gmail.com>
2018-03-19 14:35:28 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,8 +6,8 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
+        binding.pry
     self.first(rating: :desc)
-    binding.pry
 
     #Show.order(rating: :desc).limit(1)
   end
