4103af34ec1b826db38f3109bc8ef9ccbc24a627
Eden Mesfun <emesfun@gmail.com>
2018-03-19 14:32:30 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,10 +2,12 @@ require 'pry'
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    Show.order(rating: :desc).maximum(:rating)
+    self.order(rating: :desc).maximum(:rating)
   end
 
   def self.most_popular_show
+
+
     Show.order(rating: :desc).limit(1)
   end
 
